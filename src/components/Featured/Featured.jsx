e7d1cebc214e4001ab6ff7b8e7d0f23e3c4f859c
brunash <bobazso@yahoo.com>
2022-08-29 21:11:03 -0400

    started work on featured component

---
+++ src/components/Featured/Featured.jsx
@@ -0,0 +1,65 @@
+import "./Featured.scss";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featured__item">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
+          alt=""
+          className="featured__img"
+        />
+        <div className="featured__titles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featured__item">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
+          alt=""
+          className="featured__img"
+        />
+        <div className="featured__titles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featured__item">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
+          alt=""
+          className="featured__img"
+        />
+        <div className="featured__titles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featured__item">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
+          alt=""
+          className="featured__img"
+        />
+        <div className="featured__titles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+      <div className="featured__item">
+        <img
+          src="https://t-cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
+          alt=""
+          className="featured__img"
+        />
+        <div className="featured__titles">
+          <h1>Dublin</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import './Home.scss';
 import NavBar from '../../components/NavBar/NavBar';
 import Header from '../../components/Header/Header';
+import Featured from '../../components/Featured/Featured';
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
             <Header/>
+            <div className="home">
+                <Featured/>
+            </div>
         </div>
     )
 }
