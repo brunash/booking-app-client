18165d870889fe6880c770a4cab46fa0987031aa
brunash <bobazso@yahoo.com>
2022-09-11 12:11:25 -0400

    continued work on the hotels page

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import NavBar from '../../components/NavBar/NavBar';
 import './List.scss';
 import { format } from "date-fns";
 import { DateRange } from 'react-date-range';
+import { useState} from 'react';
+import { useLocation } from 'react-router-dom';
 
 const List = () => {
 
     const location = useLocation();
-
     const [destination, setDestination] = useState(location.state.destination);
     const [date, setDate] = useState(location.state.date);
     const [openDate, setOpenDate] = useState(false);
@@ -45,46 +44,55 @@ const List = () => {
               </div>
               <div className="list__search--item">
                 <label htmlFor="">Options</label>
-                <div className="list__search--option">
-                  <span className="list__search--option-text">
-                    Min price <small>per night</small>
-                  </span>
-                  <input type="number" className="list__search--option-input" />
-                </div>
-                <div className="list__search--option">
-                  <span className="list__search--option-text">
-                    Max price <small>per night</small>
-                  </span>
-                  <input type="number" className="list__search--option-input" />
-                </div>
-                <div className="list__search--option">
-                  <span className="list__search--option-text">Adult</span>
-                  <input
-                    type="number"
-                    className="list__search--option-input"
-                    min={1}
-                    placeholder={options.adult}
-                  />
-                </div>
-                <div className="list__search--option">
-                  <span className="list__search--option-text">Children</span>
-                  <input
-                    type="number"
-                    className="list__search--option-input"
-                    min={0}
-                    placeholder={options.children}
-                  />
-                </div>
-                <div className="list__search--option">
-                  <span className="list__search--option-text">Room</span>
-                  <input
-                    type="number"
-                    className="list__search--option-input"
-                    min={1}
-                    placeholder={options.room}
-                  />
+                <div className="list__container">
+                  <div className="list__search--option">
+                    <span className="list__search--option-text">
+                      Min price <small>per night</small>
+                    </span>
+                    <input
+                      type="number"
+                      className="list__search--option-input"
+                    />
+                  </div>
+                  <div className="list__search--option">
+                    <span className="list__search--option-text">
+                      Max price <small>per night</small>
+                    </span>
+                    <input
+                      type="number"
+                      className="list__search--option-input"
+                    />
+                  </div>
+                  <div className="list__search--option">
+                    <span className="list__search--option-text">Adult</span>
+                    <input
+                      type="number"
+                      className="list__search--option-input"
+                      min={1}
+                      placeholder={options.adult}
+                    />
+                  </div>
+                  <div className="list__search--option">
+                    <span className="list__search--option-text">Children</span>
+                    <input
+                      type="number"
+                      className="list__search--option-input"
+                      min={0}
+                      placeholder={options.children}
+                    />
+                  </div>
+                  <div className="list__search--option">
+                    <span className="list__search--option-text">Room</span>
+                    <input
+                      type="number"
+                      className="list__search--option-input"
+                      min={1}
+                      placeholder={options.room}
+                    />
+                  </div>
                 </div>
               </div>
+              <button>Search</button>
             </div>
             <div className="list__result"></div>
           </div>
