7de263e338b05cf15db6bcda36fbe2e5576c7a89
brunash <bobazso@yahoo.com>
2022-09-12 17:52:40 -0400

    working on individual hotel page

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -36,16 +36,49 @@ const Hotel = () => {
             <div className="hotel__address">
               <FontAwesomeIcon icon={faLocationDot} />
               <span>Elton Street 12 New York</span>
-              <span className='hotel__distance'>Excellent location - 500m from center 
+              <span className="hotel__distance">
+                Excellent location - 500m from center
               </span>
-              <span className='hotel__price-highlight'>Book a stay over $114 at this propertyu and get a free airport taxi
+              <span className="hotel__price-highlight">
+                Book a stay over $114 at this propertyu and get a free airport
+                taxi
               </span>
               <div className="hotel__images">
-                {photos.map(photo=> (
-                    <div className="hotel__image-wrapper">
-                        <img src={photo.src} alt="" className='hotel__image' />
-                    </div>
+                {photos.map((photo) => (
+                  <div className="hotel__image-wrapper">
+                    <img src={photo.src} alt="" className="hotel__image" />
+                  </div>
                 ))}
+              </div>
+              <div className="hotel__details">
+                <div className="hotel__details--text">
+                  <h1 className="hotel__title">Stay in the heart of City</h1>
+                  <p className="hotel__desc">
+                    Located a 5-minute walk from St. Florian's Gate in Krakow,
+                    Tower Street Apartments has accommodations with air
+                    conditioning and free WiFi. The units come with hardwood
+                    floors and feature a fully equipped kitchenette with a
+                    microwave, a flat-screen TV, and a private bathroom with
+                    shower and a hairdryer. A fridge is also offered, as well as
+                    an electric tea pot and a coffee machine. Popular points of
+                    interest near the apartment include Cloth Hall, Main Market
+                    Square and Town Hall Tower. The nearest airport is John Paul
+                    II International Kraków–Balice, 16.1 km from Tower Street
+                    Apartments, and the property offers a paid airport shuttle
+                    service.
+                  </p>
+                </div>
+                <div className="hotel__details--price">
+                  <h1>Perfect for a 9-night stay!</h1>
+                  <span>
+                    Located in the real heart of Krakow, this property has an
+                    excellent location score of 9.8!
+                  </span>
+                  <h2>
+                    <b>$945</b> (9 nights)
+                  </h2>
+                  <button>Reserve or Book Now!</button>
+                </div>
               </div>
             </div>
           </div>
