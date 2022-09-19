a95a753b139bff85add388a0df177a636e7fa153
brunash <bobazso@yahoo.com>
2022-09-19 19:11:38 -0400

    began working on slider component

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -4,8 +4,18 @@ import Header from "../../components/Header/Header";
 import MailList from '../../components/MailList/MailList';
 import Footer from '../../components/Footer/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCircleArrowLeft,
+  faLocationDot,
+  faCircleXmark,
+  faCircleArrowRight
+} from "@fortawesome/free-solid-svg-icons";
+import { useState } from 'react';
+
 const Hotel = () => {
+
+    const [slideNumber, setSlideNumber] = useState(0);
+    const [open, setOpen] = useState(false);
 
     const photos = [
       {
@@ -28,67 +38,86 @@ const Hotel = () => {
       },
     ];
 
+    const handleOpen = (i) => {
+      setSlideNumber(i);
+      setOpen(true);
+    }
+
     return (
       <div>
         <NavBar />
         <Header type="list" />
         <div className="hotel__container">
+          {open && (
+            <div className="slider">
+              <FontAwesomeIcon icon={faCircleXmark} />
+              <FontAwesomeIcon icon={faCircleArrowLeft} />
+              <div className="slider__wrapper">
+                <img src={photos[slideNumber].src} alt="" className='slider__image'/>
+              </div>
+              <FontAwesomeIcon icon={faCircleArrowRight} />
+            </div>
+          )}
           <div className="hotel__wrapper">
-            <button className='hotel__book-now'>Reserve or book now!</button>
+            <button className="hotel__book-now">Reserve or book now!</button>
             <h1 className="hotel__title">Grand Hotel</h1>
             <div className="hotel__address">
               <FontAwesomeIcon icon={faLocationDot} />
               <span>Elton Street 12 New York</span>
             </div>
-              <span className="hotel__distance">
-                Excellent location - 500m from center
-              </span>
-              <span className="hotel__price-highlight">
-                Book a stay over $114 at this property and get a free airport
-                taxi
-              </span>
-              <div className="hotel__images">
-                {photos.map((photo) => (
-                  <div className="hotel__image-wrapper">
-                    <img src={photo.src} alt="" className="hotel__image" />
-                  </div>
-                ))}
+            <span className="hotel__distance">
+              Excellent location - 500m from center
+            </span>
+            <span className="hotel__price-highlight">
+              Book a stay over $114 at this property and get a free airport taxi
+            </span>
+            <div className="hotel__images">
+              {photos.map((photo, i) => (
+                <div className="hotel__image-wrapper">
+                  <img
+                    onClick={()=>handleOpen(i)}
+                    src={photo.src}
+                    alt=""
+                    className="hotel__image"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="hotel__details">
+              <div className="hotel__details--text">
+                <h1 className="hotel__title">Stay in the heart of City</h1>
+                <p className="hotel__desc">
+                  Located a 5-minute walk from St. Florian's Gate in Krakow,
+                  Tower Street Apartments has accommodations with air
+                  conditioning and free WiFi. The units come with hardwood
+                  floors and feature a fully equipped kitchenette with a
+                  microwave, a flat-screen TV, and a private bathroom with
+                  shower and a hairdryer. A fridge is also offered, as well as
+                  an electric tea pot and a coffee machine. Popular points of
+                  interest near the apartment include Cloth Hall, Main Market
+                  Square and Town Hall Tower. The nearest airport is John Paul
+                  II International Kraków–Balice, 16.1 km from Tower Street
+                  Apartments, and the property offers a paid airport shuttle
+                  service.
+                </p>
               </div>
-              <div className="hotel__details">
-                <div className="hotel__details--text">
-                  <h1 className="hotel__title">Stay in the heart of City</h1>
-                  <p className="hotel__desc">
-                    Located a 5-minute walk from St. Florian's Gate in Krakow,
-                    Tower Street Apartments has accommodations with air
-                    conditioning and free WiFi. The units come with hardwood
-                    floors and feature a fully equipped kitchenette with a
-                    microwave, a flat-screen TV, and a private bathroom with
-                    shower and a hairdryer. A fridge is also offered, as well as
-                    an electric tea pot and a coffee machine. Popular points of
-                    interest near the apartment include Cloth Hall, Main Market
-                    Square and Town Hall Tower. The nearest airport is John Paul
-                    II International Kraków–Balice, 16.1 km from Tower Street
-                    Apartments, and the property offers a paid airport shuttle
-                    service.
-                  </p>
-                </div>
-                <div className="hotel__details--price">
-                  <h1>Perfect for a 9-night stay!</h1>
-                  <span>
-                    Located in the real heart of Krakow, this property has an
-                    excellent location score of 9.8!
-                  </span>
-                  <h2>
-                    <b>$945</b> (9 nights)
-                  </h2>
-                  <button>Reserve or Book Now!</button>
-                </div>
+              <div className="hotel__details--price">
+                <h1>Perfect for a 9-night stay!</h1>
+                <span>
+                  Located in the real heart of Krakow, this property has an
+                  excellent location score of 9.8!
+                </span>
+                <h2>
+                  <b>$945</b> (9 nights)
+                </h2>
+                <button>Reserve or Book Now!</button>
               </div>
             </div>
-            <MailList/>
-            <Footer/>
           </div>
+          <MailList />
+          <Footer />
         </div>
+      </div>
     );
 }
 
