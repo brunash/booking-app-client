1732fbacf30bfc8f81a6f1b3cbb0af9a3ff90dda
brunash <bobazso@yahoo.com>
2022-09-29 22:23:42 -0400

    tried something

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,10 +3,12 @@ import "./Featured.scss";
 
 const Featured = () => {
   const { data, loading, error } = useFetch(
-    "proxy/hotels/countByCity?cities=Sombor,Rome"
+    "http://localhost:8800/api/hotels/countByCity?cities=Sombor,Rome"
   );
+
+  //,"proxy":"http://localhost:8800/api" this can be put in the end in package.json
   //throwing an error when trying to console log the following
-   // could be an issue with port http://localhost:3000/proxy/hotels/countByCity?cities=Sombor,Rome
+  // could be an issue with port http://localhost:3000/proxy/hotels/countByCity?cities=Sombor,Rome
   console.log(data);
   return (
     <div className="featured">
@@ -21,7 +23,7 @@ const Featured = () => {
           <h2>123 properties</h2>
         </div>
       </div>
-      <div className="featured__item">
+      <div className="featured__item ">
         <img
           src="https://t-cf.bstatic.com/xdata/images/city/square250/690203.webp?k=dc8f5453ce80f38f94b1575299ae214a62239fd07eb6927c1518eeb703fa56c5&o="
           alt=""
