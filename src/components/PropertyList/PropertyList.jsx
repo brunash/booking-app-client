995e2c1e9b1c8a4698405144ad0699b49fca7fc2
brunash <bobazso@yahoo.com>
2022-10-04 16:32:13 -0400

    added dynamic values to property list component

--- src/components/PropertyList/PropertyList.jsx
+++ src/components/PropertyList/PropertyList.jsx
@@ -1,52 +1,39 @@
+import useFetch from "../../hooks/useFetch";
 import "./PropertyList.scss";
 
 const PropertyList = () => {
+  const { data, loading, error } = useFetch(
+    "http://localhost:8800/api/hotels/countByType"
+  );
+
+  const images = [
+    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
+    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
+  ];
+
   return (
     <div className="property">
-      <div className="property__item">
-        <img
-          className="img property__img"
-          alt=""
-          src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
-        />
-        <div className="property__titles">
-          <h1>Hotels</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
-      <div className="property__item">
-        <img
-          className="img property__img"
-          alt=""
-          src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
-        />
-        <div className="property__titles">
-          <h1>Hotels</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
-      <div className="property__item">
-        <img
-          className="img property__img"
-          alt=""
-          src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
-        />
-        <div className="property__titles">
-          <h1>Hotels</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
-      <div className="property__item">
-        <img
-          className="img property__img"
-          alt=""
-          src="https://q-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
-        />
-        <div className="property__titles">
-          <h1>Hotels</h1>
-          <h2>233 Hotels</h2>
-        </div>
-      </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className="property__item">
+                <img className="img property__img" alt="" src={img} />
+                <div className="property__titles">
+                  <h1>{data[i].type}</h1>
+                  <h2>
+                    {data[i].count} {data[i].type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
   );
 };
